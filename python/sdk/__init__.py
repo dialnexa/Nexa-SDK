33f24301086949721ff7f84750dd98694cd39ba4
SauravHeycoach <sauravheycoach@gmail.com>
2025-10-16 15:57:36 +0530

    Revert "Code changes for agents sdk"
    This reverts commit 596c8cefa7764819c285c38ad36321d058e93e3e.

--- python/sdk/__init__.py
+++ python/sdk/__init__.py
@@ -1,6 +1,5 @@
 from .languages import Language, LanguagesResponse, LanguagesClient
 from .calls import ApiResponse as CallsApiResponse, CallCreateResponse, CallsClient
-from .agents2 import ApiResponse as Agents2ApiResponse, Agents2Client
 
 __all__ = [
     "Language",
@@ -9,6 +8,4 @@ __all__ = [
     "CallsApiResponse",
     "CallCreateResponse",
     "CallsClient",
-    "Agents2ApiResponse",
-    "Agents2Client",
 ]
